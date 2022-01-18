b6c0014414803f5b5e51e4ac5c28c73ee2777d1e
Krifty <kaitlynmariejones@gmail.com>
2022-01-18 13:56:47 -0800

    working on Quest and Quest DOM display

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import _ from 'lodash';
 import "./style/style.css";
+import "./style/questList.css"
 import layout from './modules/layout/pageLayout';
 import form from "./modules/Project/form";
+import Quest from './modules/Project/quest';
 
 layout();
+Quest();
 form();
--- src/modules/Project/quest.js
+++ src/modules/Project/quest.js
@@ -1,3 +1,5 @@
+const { isObject } = require("lodash");
+
 function Quest() {
    
     class Quest {
@@ -12,9 +14,50 @@ function Quest() {
         }
     }
 
+    let questOne = new Quest();
+
+    let example1 = new Quest("blah", "2022-01-05", "blue", "no", ["html", "javascript", "css"]);
+    let example2 = new Quest("coding", "2022-01-05", "blue", "no", [example1, example1]);
+    // let example2 = new Quest(coding, "2022-01-05", blue, no, ["html", "javascript", "css"]);
+     console.log(example1);
+     console.log(example2);
+     console.log(questOne);
+
+     
+
+
+    let content = document.getElementById("content");
+    let questList = document.createElement("div");
+    questList.id = "questlist"
+    content.appendChild(questList);
+
+   
+    let titleDisplay =document.createElement("p");
+    titleDisplay.classList = "title"
+    titleDOM (titleDisplay, example2) 
+
     
+    example2.subquest.forEach(element => {  
+        let subquestDisplay = document.createElement("p");
+        subquestDisplay.classList = "subquest"
+        titleDOM (subquestDisplay, element)
+    });
 
+    //creates Checkbox, BR, adds innerText and style to each title
+    function titleDOM (variable, element) {
+        let checkbox = document.createElement("input")
+        checkbox.type = "checkbox"
+        checkbox.classList = "checkbox"
+        questList.appendChild(checkbox)
+       
+        variable.style = "text-transform:capitalize;"
+        variable.innerText = element.title;
+        questList.appendChild(variable);
 
+        let br = document.createElement("BR");
+        questList.appendChild(br);
+    }
+    
 
 }
 
